1ccd2b470637d866a637a7b7ce7209596845eb58
minseong7164 <minseong7164@naver.com>
2025-05-14 17:09:30 +0900

    feat:20250514/

---
+++ src/main/java/com/korit/demo/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.korit.demo.controller;
+
+import com.korit.demo.entity.User;
+import com.korit.demo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    // http://localhost:8080/swagger-ui/index.html
+    // http://localhost:8080/h2-console
+    /*
+        Object : 모든 자료형의 최상위 -> Object란 모든 데이터를 의미한다.
+     */
+
+    @PostMapping("/users")
+    public ResponseEntity<User> register(@RequestBody Map<String, String> dto) {
+        User user = new User();
+        user.setUsername(dto.get("username"));
+        user.setPassword(dto.get("password"));
+        return ResponseEntity.ok(userService.register(user));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUserAll() {
+        return ResponseEntity.ok(userService.findUsersAll());
+    }
+}
